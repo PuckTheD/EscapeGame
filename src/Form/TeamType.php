f25d359f3b727d9ccae13ba289cf6ecb189e0173
gandalf27-alt <59562492+gandalf27-alt@users.noreply.github.com>
2020-05-07 14:48:17 +0200

    User et Team final bis

--- src/Form/TeamType.php
+++ src/Form/TeamType.php
@@ -3,17 +3,25 @@
 namespace App\Form;
 
 use App\Entity\Team;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class TeamType extends AbstractType
+class Team1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
-            ->add('users')
+            ->add('users', EntityType::class, array(
+                'class'        => User::class,
+                'choice_label' => 'nickname',
+                'multiple'    => true,
+                'expanded'    => true,
+            ))
         ;
     }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,23 +3,41 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\Team;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class UserType extends AbstractType
+
+
+class User1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('email')
-            ->add('roles')
             ->add('password')
+            ->add('roles', ChoiceType::class, array(
+                'choices' => array(
+                    'ROLE_USER' => 'ROLE_USER',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                    'ROLE_SUPER_ADMIN' => 'ROLE_SUPER_ADMIN',
+                    ),
+                    'expanded'  => true,
+                    'multiple' => true
+                )
+            )
             ->add('nickname')
             ->add('avatar')
             ->add('token')
-            ->add('role')
-            ->add('teams')
+            ->add('teams', EntityType::class, array(
+                'class'        => Team::class,
+                'choice_label' => 'name',
+                'multiple'    => true,
+                'expanded'    => true,
+            ))
         ;
     }
 

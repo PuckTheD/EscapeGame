cee909a54f2656980b77edc8e75d0b0b413cfdc6
Alexandre HOSSEIN <alex.hos.27@gmail.com>
2020-05-12 10:44:06 +0200

    modif controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\User1Type;
+use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/user")

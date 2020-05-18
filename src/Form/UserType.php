b7f84900599c38583f533fe5cdc66b1ac76dabd5
Alexandre HOSSEIN <alex.hos.27@gmail.com>
2020-05-18 16:15:37 +0200

    modif form edit

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,11 +5,16 @@ namespace App\Form;
 use App\Entity\User;
 use App\Entity\Team;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\File;
 
 
 class UserType extends AbstractType
@@ -17,28 +22,55 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('roles', ChoiceType::class, array(
-                    'choices' => array(
-                        'ROLE_USER' => 'ROLE_USER',
-                        'ROLE_ADMIN' => 'ROLE_ADMIN',
-                        'ROLE_SUPER_ADMIN' => 'ROLE_SUPER_ADMIN',
-                    ),
-                    'expanded'  => true,
-                    'multiple' => true
-                )
-            )
-            ->add('nickname')
-            ->add('avatar')
-            ->add('token')
-            ->add('teams', EntityType::class, array(
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail'
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe n\'est pas identique.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => false,
+                'first_options' => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Confirmer nouveau mot de passe'],
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'constraints' => [
+                    new Length([
+                        'min' => 0,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 25,
+                    ]),
+                ],
+            ])
+            ->add('nickname', TextType::class, [
+                'label' => 'Pseudo'
+                ])
+            ->add('avatar', FileType::class, [
+                'label' => 'Avatar (.jpg, .jpeg, .png)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '4000k',
+                    ])
+                ],
+            ]);
+            /*->add('teams', EntityType::class, array(
                 'class'        => Team::class,
                 'choice_label' => 'name',
                 'multiple'    => true,
                 'expanded'    => true,
-            ))
-        ;
+            ));*/
     }
 
     public function configureOptions(OptionsResolver $resolver)

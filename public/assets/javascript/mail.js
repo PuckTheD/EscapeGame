0178227f6a7b2ee67233a827ce7fa74d8ced679f
Christine Venet <christinevenet@ntymail.com>
2020-05-24 13:52:03 +0200

    maj mail

--- public/assets/javascript/mail.js
+++
@@ -1,6 +0,0 @@
-
-$( document ).ready(function() {
-    $mail = $.getJSON("/data/beyond-mail.json", function (data) {
-        console.log(data);
-    });
-});
--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -2,33 +2,21 @@
 
 namespace App\Controller;
 
-use App\Entity\Mail;
-use Symfony\Component\Serializer\Encoder\ContextAwareDecoderInterface;
-use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MailController implements  SerializerInterface, ContextAwareDenormalizerInterface, ContextAwareDecoderInterface
+class MailController extends AbstractController
 {
     /**
      * @Route("/mail", name="mail")
      */
     public function index()
     {
-        $data = file_get_contents('/data/beyond-mail.json');
-        $data = $this->decode($data, 'json');
-
-        //$serializer->decode($data, 'json')
-        //$serializer->denormalize($data, 'Mail', 'json')
-        ///////////////////////////
-        //$serializer->deserialize($data, Mail::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $mail]);
-        //////////////////////////
-        //$content_beyond_mail = json_decode($json_beyond_mail, true);
-        ///////////////////:
+        $data = file_get_contents($this->getParameter('kernel.project_dir') . '/public/data/mails.json');
+        $data = json_decode($data);
         return $this->render('mail/index.html.twig', [
             'controller_name' => 'MailController',
-            $this->denormalize($data, 'Mail', 'json')
-           // 'content_beyond_mail' => $content_beyond_mail,
+            'data' => $data
         ]);
     }
 }

a946a1c8891e6232c7c4e3fda19a81e908544cc9
Laura <simon.laura.sl@hotmail.fr>
2020-05-28 01:47:47 +0200

    Modifs contenu folders + Modifs mails + màj dashboard admin

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -19,15 +19,24 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = new User();
-        $user->setEmail('alex.hos.27@gmail.com');
+        $user->setEmail('simon.laura.sl@hotmail.fr');
         $user->setNickname('Admin');
         $user->setAvatar('AdminIcon.jpeg');
         $user->setRoles(array('ROLE_ADMIN'));
         $user->setPassword($this->passwordEncoder->encodePassword($user, 'admin'));
         $manager->persist($user);
 
+        $user2 = new User();
+        $user2->setEmail('laurasimon@hotmail.fr');
+        $user2->setNickname('Player1');
+        $user2->setAvatar('DefaultAvatar.jpeg');
+        $user2->setRoles(array('ROLE_USER'));
+        $user2->setPassword($this->passwordEncoder->encodePassword($user2, 'player'));
+        $manager->persist($user2);
+
         $manager->flush();
 
         $this->addReference('UserAdmin', $user);
+        $this->addReference('User', $user2);
     }
 }

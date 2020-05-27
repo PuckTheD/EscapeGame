643d184e5a5ae1eb2bb9101d29a4276d5d5a4457
Christine Venet <christinevenet@ntymail.com>
2020-05-27 11:19:52 +0200

    lettre mission

--- public/assets/javascript/folders.js
+++ public/assets/javascript/folders.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
+
+
+
     $( ".hint" ).hide();
     $( ".help" ).click(function() {
         $( ".hint" ).toggle();
     });
-    // Validation du formulaire Apparition du contenu
-});
